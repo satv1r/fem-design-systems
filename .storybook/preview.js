1e1c7aa418bf836294e558c2281447560a291cad
Satvir Sandhu <356Sandhu@gmail.com>
2020-10-20 16:38:11 -0400

    add actions, knobs and stuff

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,14 @@
+import { addParameters, addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { contexts } from "./contexts";
+import { withKnobs } from "@storybook/addon-knobs";
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+addParameters({
+  backgrounds: [
+    { name: "Default Theme", value: "#ffffff", default: true },
+    { name: "Dark Theme", value: "#050449" },
+  ],
+});
+
+addDecorator(withContexts(contexts));
+addDecorator(withKnobs);
